6fbe23639beaf3858b6c61ac67d0cf7ad6c27e74
Dan Lavine <lavineds@gmail.com>
2022-12-03 19:30:30 -0800

    Add an interface for TaskManager
    * Generate fakes for all the interfaces

--- task.go
+++ task.go
@@ -4,6 +4,20 @@ import "context"
 
 //go:generate go run github.com/maxbrunsfeld/counterfeiter/v6 -generate
 
+// TaskManager manages any number of tasks
+//
+//counterfeiter:generate . TaskManager
+type TaskManager interface {
+	// Add a task before running the task manager
+	AddTask(name string, task Task) error
+
+	// add a task to an already running task manager
+	AddRunningTask(name string, task RunningTask) error
+
+	// run the task manager
+	Run(context context.Context) []NamedError
+}
+
 // A Task is anything that can be managed by the TaskManager and added before the taskmanager
 // start running. Any errors will cause the process to exit as all tasks are expected to run without erros
 //
@@ -24,6 +38,8 @@ type Task interface {
 
 // A RunningTask can be added to a Task Manager after it has already started managin the tasks.
 // These tasks are expected to already be properly Initialized and don't require any Cleanup Code.
+//
+//counterfeiter:generate . RunningTask
 type RunningTask interface {
 	// Execute is the main Async function to house all the multi-threaded logic handled by GoAsync.
 	Execute(ctx context.Context) error
