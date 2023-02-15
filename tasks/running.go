edbfc4ff4e56e7cb3a6936de1d5c024a7a60bb83
Dan Lavine <lavineds@gmail.com>
2023-02-15 15:29:37 -0800

    Add a running task to know if the task manager has started

---
+++ tasks/running.go
@@ -0,0 +1,31 @@
+package tasks
+
+import (
+	"context"
+)
+
+type running struct {
+	started chan struct{}
+}
+
+// Running can be used to to if a TaskManager has started executing background threads. This
+// is useful to know is a task manager received a HUP operation and all tasks were restared.
+// passing in a new Running task will let us know when the task manager has started again
+//
+// NOTE: that this subtask will exit so its intended to be used with a "Relaxed" task manager.
+//
+// Args:
+// * started - chan that will be closed when Execute(...) is called
+func Running(started chan struct{}) *running {
+	return &running{
+		started: started,
+	}
+}
+
+func (r *running) Initialize() error { return fs.subTask.Initialize() }
+func (r *running) Cleanup() error    { return fs.subTask.Cleanup() }
+func (r *running) Execute(ctx context.Context) error {
+	close(r.started)
+
+	return
+}
